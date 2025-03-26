6f940a906911ac8f65bca2a3c83a68b13b0d61c8
kevdbowers <kevdbowers@gmail.com>
2025-03-26 10:54:47 -0400

    updated .gitignore, updated main.py with game window initialization

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import pygame  #importing necessary libraries, and all magic numbers from consta
 from constants import *
 
 def main():  #primary function designed to run asteroids
+    pygame.init()  #initializing all imported pygame modules
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  #creating game window
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:  #running asteroids
+        for event in pygame.event.get():  #enabling close button in window
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip  #refreshing game window
+
 
 if __name__ == "__main__":  #call to main that runs asteroids when this file is run directly
     main()
