3bb3c6f2d099aa1522e3936e70dbdadc639d08e3
kevdbowers <kevdbowers@gmail.com>
2025-03-26 10:24:54 -0400

    updated README.md, updated main.py, added constants.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import pygame  #importing necessary library
+import pygame  #importing necessary libraries, and all magic numbers from constants.py
+from constants import *
 
 def main():  #primary function designed to run asteroids
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":  #call to main that runs asteroids when this file is run directly
     main()
