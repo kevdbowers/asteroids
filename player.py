0f68821513f0f589977a86e27de144df823df4fc
kevdbowers <kevdbowers@gmail.com>
2025-04-04 11:13:39 -0400

    updated player.py with an acceleration mechanic for the move method, updated constants.py

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):  #creating player subclass of CircleShape
         self.shot_timer = 0
         self.lives = 3
         self.invuln_timer = PLAYER_INVULN_TIMER
+        self.acceleration = 0
 
     def triangle(self):  #method to define player visual
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -33,10 +34,12 @@ class Player(CircleShape):  #creating player subclass of CircleShape
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)
+        if keys[pygame.K_w] and not keys[pygame.K_s]:
+            self.acceleration += 0.04
+        if keys[pygame.K_s] and not keys[pygame.K_w]:
+            self.acceleration -= 0.02
+
+        self.move(dt)
         
         if keys[pygame.K_SPACE]:
             self.shoot()
@@ -44,9 +47,19 @@ class Player(CircleShape):  #creating player subclass of CircleShape
         if self.invuln_timer > 0:
             self.invuln_timer -= 1        
 
-    def move(self, dt):  #method to adjust player position
+    def move(self, dt):  #method to adjust player position and acceleration
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        if self.acceleration > 1:
+            self.acceleration = 1
+        if self.acceleration < -0.5:
+            self.acceleration = -0.5
+
+        self.position += forward * PLAYER_SPEED * self.acceleration * dt
+
+        if self.acceleration > 0:
+            self.acceleration -= 0.01
+        if self.acceleration < 0:
+            self.acceleration += 0.01
 
     def shoot(self):  #method to create shots from the player
         if self.shot_timer > 0:
